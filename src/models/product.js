87f734bfeb7a4b58347460fffa813890ddffb74d
Ambica <ambicana5@gmail.com>
2022-10-17 14:49:27 +0545

    created mangoose schema for user registration

--- src/models/product.js
+++ src/models/product.js
@@ -8,10 +8,21 @@ const ProductSchema = new mongoose.Schema({
     category: {type: String, required: true},
     id: {type: String},
     created: {type: String},
-	isLiked: {type: Boolean, default: false}
+	isLiked: {type: Boolean,}
     
 }, {
     collection: 'products'
 })
+
+const RegisterSchema = new mongoose.Schema({
+    name: {type: String, required:  true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    
+}, {
+    collection: 'register'
+})
+
 const Products = mongoose.model('ProductModel', ProductSchema)
+const Register = mongoose.model('RegissterModel', RegisterSchema)
 module.exports = Products
